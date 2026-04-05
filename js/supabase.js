33c1cb6f52c01854211898e8814b08b1975cd8cd
Jingalalala <97291852+Jingalalala@users.noreply.github.com>
2026-04-05 16:52:53 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://aqecgofdcngooqcxdieu.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZWNnb2ZkY25nb29xY3hkaWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNTAyNDQsImV4cCI6MjA5MDYyNjI0NH0.HbsoNGvT6i3lERkskdrJfV1f4WLKT8dzRfaXtPJnJ7k";
+
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
