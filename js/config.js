eecce4c06c885798a518467f83873683a95c8c48
Jingalalala <97291852+Jingalalala@users.noreply.github.com>
2026-04-05 16:24:08 +0530

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.SUPABASE_URL = "https://aqecgofdcngooqcxdieu.supabase.co";
-window.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxZWNnb2ZkY25nb29xY3hkaWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNTAyNDQsImV4cCI6MjA5MDYyNjI0NH0.HbsoNGvT6i3lERkskdrJfV1f4WLKT8dzRfaXtPJnJ7k";
-
-window.supabaseClient = window.supabase.createClient(
-  window.SUPABASE_URL,
-  window.SUPABASE_KEY
-);
